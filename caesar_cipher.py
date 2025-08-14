c4d6e130fdb8c5d3144dccf537ef19edcb94f01e
Manish Dey <manishdey356@gmail.com>
2025-08-15 02:07:50 +0530

    added modulo operator
    To wrap text
    error-list index out of range

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,22 +1,21 @@
 #Caesar Cipher-Task1
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m',
           'n','o','p','q','r','s','t','u','v','w','x','y','z']
-def encryption(plaintext,shiftkey):#hello
+def encryption(plaintext,shiftkey):
     ciphertext=""
-    for char in plaintext:          #to get the index
+    for char in plaintext:          
         position=alphabet.index(char)
-        newposition=position+shiftkey
+        newposition=(position+shiftkey)%26
         ciphertext+=alphabet[newposition]
     print(f"The text after encryption is: {ciphertext}")
 
 
+
     
-
-
 option=input("Type 'e' for encryption and 'd' for decryption:\n")
 text=input("Type your message:")
 shift=int(input("Enter the shift key:\n"))
 if option=="e":
     encryption(plaintext=text,shiftkey=shift)
 #elif option=="d":
- #   decryption(ciphertext=text,shiftkey=shift)
+#  decryption(ciphertext=text,shiftkey=shift)
